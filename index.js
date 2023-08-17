850f04fa5195763285e9fd47cf84e9a6a7fcb0b0
Baz Rahimi <bazrahimi@hotmail.com>
2023-08-18 01:16:39 +1000

    added.js

--- index.js
+++ index.js
@@ -2,6 +2,42 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
+const promptUser = async() => {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter your Logo-text. (should exceed three characters.',
+      validate: (input) => {
+        if (input.length > 3) {
+          return 'Please enter not more than three chracters'
+        }
+        return true;
+      },
+
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter a text color (OR a hexadecimal number)',
+
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape:',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter the shape color (keyword or hex):',
+    },
+  ]);
+
+  return answers;
+};
+
 
 
 
